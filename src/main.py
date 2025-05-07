01e697ca34657436ebc88dd3d125d66a5a929126
Grace Dmello <77654364+sea-shanties@users.noreply.github.com>
2025-05-07 17:58:40 -0500

    initial setup complete

--- src/main.py
+++ src/main.py
@@ -3,7 +3,35 @@ import sys
 
 class App:
     def __init__(self):
+        # initialize the window and set a clock in order to update the game
         pg.init()
         pg.display.set_caption("Tetris")
         self.screen = pg.display.set_mode(FIELD_RES)
         self.clock = pg.time.Clock()
+    
+    def update(self):
+        # set the framerate- FPS var created in settings.py
+        self.clock.tick(FPS)
+    
+    def draw(self):
+        # draw & display the background
+        self.screen.fill(color=FIELD_COLOR)
+        pg.display.flip
+    
+    def check_events(self):
+        # check if user quits the game, and exit accordingly
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+    
+    def run(self):
+        # main game loop
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,8 @@
 import pygame as pg
 
+# this file includes the base settings for the game; including the
+# tile & field size, and the background color of the game.
+
 FPS = 60
 FIELD_COLOR = (48, 39, 32)
 
