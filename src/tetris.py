d4d6607654df030dec52f77f885c268d287fc399
Grace Dmello <77654364+sea-shanties@users.noreply.github.com>
2025-05-07 20:40:00 -0500

    added code to draw tetris grid

--- src/tetris.py
+++ src/tetris.py
@@ -6,8 +6,13 @@ class Tetris:
     def __init__(self, app):
         self.app = app
     
+    def draw_grid(self):
+        for x in (FIELD_W):
+            for y in (FIELD_H):
+                pg.draw.rect(self.app.screen, 'white', (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
+    
     def update(self):
         pass
 
     def draw(self):
-        pass
+        self.draw_grid()
