bf2c7e56ffb07eef900a845a620efb7feae3b054
sea-shanties <grace1.dmello@gmail.com>
2025-05-12 13:22:34 -0500

    created tetromino shapes

--- src/settings.py
+++ src/settings.py
@@ -1,12 +1,25 @@
-import pygame as pg
-
 # this file includes the base settings for the game; including the
 # tile & field size, and the background color of the game.
+
+import pygame as pg
+
+vec = pg.math.Vector2
 
 FPS = 60
 FIELD_COLOR = (24, 45, 84)
 
 TILE_SIZE = 30
 FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
-
 FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
+
+INIT_POS_OFFSET = vec(FIELD_SIZE) // 2
+
+TETROMINOES = {
+    'T': [(0, 0), (-1, 0), (1, 0), (0, -1)],
+    'O': [(0, 0), (0, -1), (1, 0), (1, -1)],
+    'J': [(0, 0), (-1, 0), (0, -1), (0, -2)],
+    'L': [(0, 0), (1, 0), (0, -1), (0, -2)],
+    'I': [(0, 0), (0, 1), (0, -1), (0, -2)],
+    'S': [(0, 0), (-1, 0), (0, -1), (1, -1)],
+    'Z': [(0, 0), (1, 0), (0, -1), (-1, -1)],
+}
