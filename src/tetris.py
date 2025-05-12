fa8714d1d2600addaa39a8ebb01557e4b7e0b004
sea-shanties <grace1.dmello@gmail.com>
2025-05-12 14:57:59 -0500

    test?

--- src/tetris.py
+++ src/tetris.py
@@ -7,6 +7,7 @@ class Tetris:
     def __init__(self, app):
         self.app = app
         self.sprite_group = pg.sprite.Group()
+        self.field_array = self.get_field_array
         self.field_array = self.get_field_array()
         self.tetromino = Tetromino(self)
     
@@ -18,8 +19,17 @@ class Tetris:
     def get_field_array(self):
         return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
     
+    def put_tetromino_blocks_in_array(self):
+        for block in self.tetromino.blocks:
+            x, y = int(block.pos.x), int(block.pos.y)
+            self.field_array[y][x] = block
+    
+    def get_field_array(self):
+        return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
+    
     def check_tetromino_landing(self):
         if self.tetromino.landing:
+            self.put_tetromino_blocks_in_array()
             self.put_tetromino_blocks_in_array()
             self.tetromino = Tetromino(self)
     
